640ce0a1b28e8004acf804259c2711f7de83c850
Nick <nickfp1985@users.noreply.github.com>
2017-09-18 13:22:28 -0700

    update media.py
    added comments to media.py

--- media.py
+++ media.py
@@ -2,13 +2,15 @@ import webbrowser
 
 
 class Movie():
-    """ this class stores movie information """
+    """ stores movie info and produces trailers """
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
+        """ stores movie info given in entertainment_center.py """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """ opens the movie trailer when its poster is clicked """
         webbrowser.open(self.trailer_youtube_url)
